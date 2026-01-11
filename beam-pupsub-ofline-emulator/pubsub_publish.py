4a95fe3f551c339f34d97c54040ed114ee23ab95
vigneshmailappan <107859572+vigneshmailappan@users.noreply.github.com>
2026-01-11 20:01:47 +0100

    Delete beam-pupsub-ofline-emulator directory

--- beam-pupsub-ofline-emulator/pubsub_publish.py
+++
@@ -1,9 +0,0 @@
-import pubsub_emulate as pse
-
-print("Publishing test message...")
-
-future = pse.publisher.publish(
-    pse.topic_path,
-    b'{"score": 123}',
-)
-print("Published message ID:", future.result())
